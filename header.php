04c45fa2039a320f2ef2a7d970262e2f03affe3f
Lasse Lykke <mail@lasselykke.com>
2023-10-11 21:46:00 +0200

    added googlefont and added typo to stylesheet

--- header.php
+++ header.php
@@ -8,6 +8,9 @@ include 'connection.php';
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
     <link href="./style/base.css" type="text/css" rel="stylesheet">
     <link href="./style/components.css" type="text/css" rel="stylesheet">
     <link href="./style/layout.css" type="text/css" rel="stylesheet">
