7f444c1160cac905ddd3678fe6fdfe94bdfc05ab
Lasse Lykke <mail@lasselykke.com>
2024-05-12 21:04:00 +0200

    added output files for bånd and rammer. Need to make a search function inside

--- output_rammer.php
+++ output_rammer.php
@@ -23,71 +23,85 @@ if (isset($_SESSION['users_id']) && isset($_SESSION['user_name'])) {
             </div>
 
             <div class="søge-resultat">
+
+            <form method="POST" action="output_rammer.php">
+                <input type="text" name="søgeord" placeholder="Søg efter ordre">
+                <button type="submit" name="søg">Søg</button>
+            </form>
             <?php
 
+    // Standard SQL-forespørgsel for at hente alle ordre
+    $sql = "SELECT * FROM ramme";
+    $result = mysqli_query($conn, $sql);
+    $queryResult = mysqli_num_rows($result);
 
-
-    $sql = "SELECT ramme.rammeID, ramme.profil, ramme.dates, ramme.profil, ramme.størrelse, ramme.glastype, ramme.passepartout, ramme.hulmål, 
-    ramme.passepartoutFarve, ramme.antal, ramme.montering, ramme.billedetype, ramme.bemærkninger, ramme.ekspedient
-    FROM ramme 
-    WHERE ramme.dates";
+    // Check if search form is submitted
+    if(isset($_POST['søg'])) {
+        $search = mysqli_real_escape_string($conn, $_POST['søgeord']);
+        // Modify the SQL query to filter results based on search criteria
+        $sql = "SELECT * FROM ramme WHERE 
+                profil LIKE '%$search%' OR 
+                størrelse LIKE '%$search%' OR 
+                glastype LIKE '%$search%' OR 
+                passepartout LIKE '%$search%' OR 
+                bemærkninger LIKE '%$search%' OR 
+                ekspedient LIKE '%$search%' DESC";
+    }
 
     $result = mysqli_query($conn, $sql);
     $queryResult = mysqli_num_rows($result);
 
+    // Vis antallet af resultater
+    echo '<div class="resultat">';
+    echo "Der er " . $queryResult . " ordre";
+    echo '</div>';
+
+    // Vis tabel med alle ordre
+    echo '<div class="søge-resultat">';
     echo '<table>
-     <tr>
-    <th> Ordre </th> 
-    <th> Dato </th>
-    <th> Rammeprofil </th>
-    <th> Størrelse </th>
-    <th> Glas </th>
-    <th> Passepartout </th>
-    <th> Hulmål </th>
-    <th> PP Farve </th>
-    <th> Antal </th>
-    <th> Montering </th>
-    <th> Billede </th>
-    <th> Bemærkninger </th>
-    <th> Ekspedient </th>
-    </tr>
-    ';
-    ?>
-    
-    <!-- Viser hvor mange resultater der er. -->
-    <div class="resultat">
-    <?php echo "Der er " . $queryResult . " ordre";?>
-</div>
-
-<?php
-    if ($queryResult > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo '<tr> 
-            <td>' . $row["rammeID"] . '</td>
-            <td>' . $row["dates"] . '</td>
-            <td> ' . $row["profil"] . '</td> 
-            <td> ' . $row["størrelse"] . '</td> 
-            <td> ' . $row["glastype"] . '</td> 
-            <td> ' . $row["passepartout"] . '</td> 
-            <td> ' . $row["hulmål"] . '</td> 
-            <td> ' . $row["passepartoutFarve"] . '</td> 
-            <td> ' . $row["antal"] . '</td> 
-            <td> ' . $row["montering"] . '</td> 
-            <td> ' . $row["billedetype"] . '</td> 
-            <td> ' . $row["bemærkninger"] . '</td> 
-            <td> ' . $row["ekspedient"] . '</td> 
+            <tr>
+                <th>Ordre</th> 
+                <th>Dato</th>
+                <th>Rammeprofil</th>
+                <th>Størrelse</th>
+                <th>Glas</th>
+                <th>Passepartout</th>
+                <th>Hulmål</th>
+                <th>PP Farve</th>
+                <th>Antal</th>
+                <th>Montering</th>
+                <th>Billede</th>
+                <th>Bemærkninger</th>
+                <th>Ekspedient</th>
             </tr>';
-        }
-        echo '</table>';
-    } 
- 
 
-mysqli_free_result($result);
+    // Vis hver ordre som en række i tabellen
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<tr> 
+                <td>' . $row["rammeID"] . '</td>
+                <td>' . $row["dates"] . '</td>
+                <td>' . $row["profil"] . '</td> 
+                <td>' . $row["størrelse"] . '</td> 
+                <td>' . $row["glastype"] . '</td> 
+                <td>' . $row["passepartout"] . '</td> 
+                <td>' . $row["hulmål"] . '</td> 
+                <td>' . $row["passepartoutFarve"] . '</td> 
+                <td>' . $row["antal"] . '</td> 
+                <td>' . $row["montering"] . '</td> 
+                <td>' . $row["billedetype"] . '</td> 
+                <td>' . $row["bemærkninger"] . '</td> 
+                <td>' . $row["ekspedient"] . '</td> 
+            </tr>';
+    }
+    echo '</table>';
+    echo '</div>'; // Lukker søge-resultat
+    echo '</div>'; // Lukker søge-wrapper
 
-// Lukker forbindelsen.
-mysqli_close($conn);
+    mysqli_free_result($result);
+    mysqli_close($conn);
 }
-    ?>
+?>
+
 
             </div> <!-- Lukker resultat -->
         </div> <!--Lukker wrapper-->
